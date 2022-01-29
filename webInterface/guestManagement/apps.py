33d103ec63ac454c1d183b0c5ad7509f5b2a8184
tetrew88 <donovanmaurice@laposte.net>
2022-01-29 12:11:49 +0100

    last update

---
+++ webInterface/guestManagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GuestmanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'guestManagement'
---
+++ webInterface/homeAutomationNetworkManagement/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HomeautomationnetworkmanagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'homeAutomationNetworkManagement'
