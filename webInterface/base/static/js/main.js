9a418e711190a160393702d7b41bb3ee28bcd113
tetrew88 <donovanmaurice@laposte.net>
2022-02-09 17:53:03 +0100

    last update

--- webInterface/base/static/js/main.js
+++ webInterface/base/static/js/main.js
@@ -3,4 +3,4 @@ console.log('hello world')
 
 var socket = io.connect("http://127.0.0.1:5000", { transports: ['websocket', 'polling', 'flashsocket'] })
 
-socket.broadcast.emit('test', 'test')
+socket.emit('test', 'test')
