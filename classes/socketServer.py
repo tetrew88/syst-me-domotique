18acf731cddb4eb62f79caa9cf927dc8241388a5
tetrew88 <donovanmaurice@laposte.net>
2022-02-09 19:26:17 +0100

    last update

--- classes/socketServer.py
+++ classes/socketServer.py
@@ -1,6 +1,8 @@
 import eventlet
 import socketio
 
+socketIoServer = socketio.Server(cors_allowed_origins="*")
+app = socketio.WSGIApp(socketIoServer)
 
 class SocketServer:
 	"""
@@ -14,18 +16,21 @@ class SocketServer:
 			methods:
 	"""
 
+	print(socketIoServer.__dict__)
+	print(app.__dict__)
+
 	def __init__(self):
-		self.socketIoServer = socketio.Server(cors_allowed_origins="*")
-		self.app = socketio.WSGIApp(socketIoServer)
+		self.test="...............!!!!!!!!!!........"
 
 
 	def start(self):
 		eventlet.wsgi.server(eventlet.listen(('', 5000)), app)
 
 
-	self.socketIoServer.event
+	@socketIoServer.event
 	def connect(sid, environ, auth):
 		print('connect ', sid)
+		print(test)
 
 
 #eesayer de nmettre la section eventlet dans home automation system pour que l'attente ce fasse au niveau du systeme et non du sserver
