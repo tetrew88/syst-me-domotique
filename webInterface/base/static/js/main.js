192703fd7b51241ecbd4eda706e2b3d1daa71065
tetrew88 <donovanmaurice@laposte.net>
2022-02-09 16:53:51 +0100

    last update

--- webInterface/base/static/js/main.js
+++ webInterface/base/static/js/main.js
@@ -1,9 +1,4 @@
 console.log('hello world')
 
 
-const io = required("socket.io")(httpServer, {
-  cors: {
-    origin: "http://127.0.0.1:5000",
-    methods: ["GET", "POST"]
-  }
-});
+const socket = io("http://127.0.0.1:5000")
