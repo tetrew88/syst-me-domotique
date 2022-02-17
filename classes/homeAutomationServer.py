327bd1eccd9b4d77593a4da9ed0388b7225d0cde
tetrew88 <donovanmaurice@laposte.net>
2022-02-18 00:30:51 +0100

    last update

--- classes/homeAutomationServer.py
+++ classes/homeAutomationServer.py
@@ -67,7 +67,7 @@ class HomeAutomationServer(socketio.Namespace):
 
 		print(rooms)
 
-		socketIoServer.emit('post_rooms_list', {'data', rooms, namespace='/HomeAutomationServer')
+		socketIoServer.emit('post_rooms_list', {'data', rooms}, namespace='/HomeAutomationServer')
 
 
 
