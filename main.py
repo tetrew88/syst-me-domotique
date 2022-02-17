78aaba90341e651d1b1a72afe2686a447ade5000
tetrew88 <donovanmaurice@laposte.net>
2022-02-17 04:55:52 +0100

    last update

--- main.py
+++ main.py
@@ -569,7 +569,7 @@ def main():
                         print("selectionner un module\n")
                         for module in homeAutomationSystem.get_home_automation_modules():
                             print("{}: {}".format(x, module.name))
-                            print(module.moduleNode.get_sensors().label)
+                            print(module.moduleNode.get_sensors().keys)
                             x += 1
 
                         tmpModuleChoice = input('Entrer votre choix: ')
