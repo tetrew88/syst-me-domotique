8f7a49af8209363c01ab4a4207b048ed6a070577
tetrew88 <donovanmaurice@laposte.net>
2022-02-18 23:40:32 +0100

    last update

--- webInterface/roomManagement/static/js/roomListing.js
+++ webInterface/roomManagement/static/js/roomListing.js
@@ -2,15 +2,12 @@ let roomList = [];
 
 socket.emit('get_rooms_list', 'rooms')
 socket.on('post_rooms_list', data=>{
-
-	roomList = data['data'];
+	for (const element of data['data']) {
+		console.log(element);
+	}
 })
 
 console.log(roomList)
-
-for (const element of roomList) {
-	console.log(element);
-}
 //const contentZone = document.getElementByClassName('content');
 //const roomCard = document.createElement('div');
 
