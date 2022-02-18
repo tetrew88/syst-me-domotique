68c606f7c7899a9f048d6c0552cb5e188e8c87e2
tetrew88 <donovanmaurice@laposte.net>
2022-02-19 00:03:39 +0100

    last update

--- webInterface/roomManagement/static/js/roomListing.js
+++ webInterface/roomManagement/static/js/roomListing.js
@@ -1,14 +1,28 @@
-let roomList = [];
+const contentZone = document.getElementById('screenContent');
+
 
 socket.emit('get_rooms_list', 'rooms')
 socket.on('post_rooms_list', data=>{
+
 	for (const element of data['data']) {
 		console.log(element);
+
+		let roomCard = document.createElement('div');
+		let cardPicture = document.createElement('img');
+		let cardTitle =  document.createElement('div');
+
+		roomCard.classList.add("col-lg-4 card roomCard rounded");
+
+		cardPicture.classList.add("img-fluid rounded");
+		cardPicture.src = "static/pictures/" + element[2];
+
+		cardTitle.classList.add("card-title text-center");
+		cardTitle.style.color = blue;
+		cardTitle.textContent = element[1]
+
+		roomCard.appendChild(cardPicture);
+		roomCard.appendChild(cardTitle);
+
+		contentZone.appendChild(roomCard);
 	}
 })
-
-console.log(roomList)
-//const contentZone = document.getElementByClassName('content');
-//const roomCard = document.createElement('div');
-
-//roomCard.classList.add("col-lg-4 card roomCard rounded")
