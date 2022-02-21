9e1c9cf5d6a65987e6bdde1f4056e49f8c5dce00
tetrew88 <donovanmaurice@laposte.net>
2022-02-22 00:59:34 +0100

    last update

--- webInterface/webInterface/urls.py
+++ webInterface/webInterface/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.conf.urls import url
 
 from authentification.views.identification import identification
