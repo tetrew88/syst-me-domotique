9dc9fa65def812ee866aa7fe46413382485bb05f
tetrew88 <donovanmaurice@laposte.net>
2022-02-22 08:30:45 +0100

    last update

--- webInterface/roomManagement/static/js/room.js
+++ webInterface/roomManagement/static/js/room.js
@@ -1,6 +1,33 @@
 let roomId = document.getElementById("roomId").value;
 
+let temperatureIndicator = document.getElementById('temperature');
+
+let luminosityIndicator = document.getElementById('luminosity');
+
 socket.emit('get_room', roomId)
 socket.on('post_room', data=>{
+	let indicatorColor = ""
+
+	data = data["data"]
+
+	temperatureIndicator.textContent = data["temperature"];
+
+	luminosityIndicator.textContent = data['luminosity'];
+
+	if( data["temperature"] < 20)
+	{
+		indicatorColor = 'blue';
+	}
+	else if(data['temperature'] < 30)
+	{
+		indicatorColor = 'green';
+	}
+	else
+	{
+		indicatorColor = 'red';
+	}
+
+	temperatureIndicator.style.color = indicatorColor;
+	luminosityIndicator.style.color = indicatorColor;
 
 })
