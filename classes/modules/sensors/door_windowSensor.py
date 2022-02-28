03935f59f2491afdee62d6f04fb4c847c7b2dca2
tetrew88 <donovanmaurice@laposte.net>
2022-02-28 16:28:16 +0100

    last update

--- classes/modules/sensors/door_windowSensor.py
+++ classes/modules/sensors/door_windowSensor.py
@@ -15,18 +15,18 @@ class Door_WindowSensor(Sensor):
 		Sensor.__init__(self, moduleNode)
 
 	@property
-    def door_windowState(self):
-    	valueId = False
+	def door_windowState(self):
+		valueId = False
 
-    	for value in self.moduleNode.get_values():
-            if self.moduleNode.get_values()[value].label == 'Access Control':
-            	valueId = value
-            	break
+		for value in self.moduleNode.get_values():
+			if self.moduleNode.get_values()[value].label == 'Access Control':
+				valueId = value
+				break
 
-        if valueId != False:
-            if self.moduleNode.get_values()[value].data == 23:
-            	return 'closed'
-            elif self.moduleNode.get_values()[value].data == 22:
-            	return 'open'
-        else:
-        	return 'None'
+		if valueId != False:
+			if self.moduleNode.get_values()[value].data == 23:
+				return 'closed'
+			elif self.moduleNode.get_values()[value].data == 22:
+				return 'open'
+		else:
+			return 'None'
