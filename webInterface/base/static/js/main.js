2e036e3e23ad36f69905f3c26cbba5c68651180f
tetrew88 <donovanmaurice@laposte.net>
2022-03-09 21:03:14 +0100

    last update

--- webInterface/base/static/js/main.js
+++ webInterface/base/static/js/main.js
@@ -1,1 +1,1 @@
-var socket = io.connect("http://141.101.68.218:5000/HomeAutomationServer", { transports: ['websocket', 'polling', 'flashsocket'] })
+var socket = io.connect("http://78.232.229.177:5000/HomeAutomationServer", { transports: ['websocket', 'polling', 'flashsocket'] })
