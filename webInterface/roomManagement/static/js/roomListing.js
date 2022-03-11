5ca9dd5b95eafd465bc4a9c7ee178b47a9ab936e
tetrew88 <donovanmaurice@laposte.net>
2022-03-11 05:48:36 +0100

    last update

--- webInterface/roomManagement/static/js/roomListing.js
+++ webInterface/roomManagement/static/js/roomListing.js
@@ -19,7 +19,7 @@ socket.on('post_rooms_list', data=>{
 			let cardTitle =  document.createElement('div');                                                                                         
 
 			link.href = '/room/' + element['id'] + "/"
-			link.classList.add("col-4 col-md-6 col-sm-12", "container-fluid")
+			link.classList.add("col-4", "col-md-6", "col-sm-12", "container-fluid")
 
 			roomCard.classList.add("card", "roomCard", "rounded");
 
@@ -91,7 +91,7 @@ socket.on('post_rooms_list', data=>{
 			let cardTitle =  document.createElement('div');
 
 			link.href = '/room/' + element['id'] + "/"
-			link.classList.add("col-4 col-md-6 col-sm-12")
+			link.classList.add("col-4 ", "col-md-6", "col-sm-12")
 
 			roomCard.classList.add("card", "roomCard", "rounded", "container-fluid");
 
