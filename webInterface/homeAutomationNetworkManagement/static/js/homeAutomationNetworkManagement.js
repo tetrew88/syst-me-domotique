0356edad50d7b08b2857afb28ee2ac1034e49903
tetrew88 <donovanmaurice@laposte.net>
2022-03-25 10:51:05 +0100

    last update

--- webInterface/homeAutomationNetworkManagement/static/js/homeAutomationNetworkManagement.js
+++ webInterface/homeAutomationNetworkManagement/static/js/homeAutomationNetworkManagement.js
@@ -19,6 +19,33 @@ socket.on('post_network_state', data=>{
 
 	let networkStateEmplacement = document.getElementById("networkState");
 
-	networkStateEmplacement.textContent = data;
+	if (data == '0')
+	{
+		networkStateEmplacement.textContent = "Arrété";
+	}
+	else if (data == '1')
+	{
+		networkStateEmplacement.textContent = "Erreur";
+	}
+	else if (data == '3')
+	{
+		networkStateEmplacement.textContent = "Redémarrer";
+	}
+	else if (data == '5')
+	{
+		networkStateEmplacement.textContent = "Démarrer";
+	}
+	else if (data == '5')
+	{
+		networkStateEmplacement.textContent = "Réveiller";
+	}
+	else if (data == '10')
+	{
+		networkStateEmplacement.textContent = "Prêt";
+	}
+	else
+	{
+		networkStateEmplacement.textContent = data;
+	}
 
 })
