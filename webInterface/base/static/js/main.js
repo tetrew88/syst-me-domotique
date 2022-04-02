9a3709afdc7c47b8b23a7ce1e50aa7c0c337d1a5
tetrew88 <donovanmaurice@laposte.net>
2022-04-02 18:39:20 +0200

    last update

--- webInterface/base/static/js/main.js
+++ webInterface/base/static/js/main.js
@@ -1,1 +1,6 @@
 var socket = io.connect("http://192.168.0.15:5000/HomeAutomationServer", { transports: ['websocket', 'polling', 'flashsocket'] })
+
+function pause(ms) 
+{
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
--- webInterface/homeAutomationNetworkManagement/static/js/addModule.js
+++ webInterface/homeAutomationNetworkManagement/static/js/addModule.js
@@ -33,7 +33,17 @@ function add_module()
 	let nameInput = document.getElementById("moduleName");
 	let locationInput = document.getElementById("moduleEmplacement");
 
-	let data = {}
+	let previousModuleList = [];
+	let newModuleList = [];
+
+	let data = {};
+	let succes = false
+
+	socket.emit('get_modules_list', '');
+	socket.on('post_modules_list', data=>{
+		previousModuleList = data["data"];
+	})
+
 
 	data['moduleName'] = nameInput.value;
 	data['roomId'] = locationInput.value;
@@ -41,4 +51,45 @@ function add_module()
 	socket.emit('add_module', data);
 	pageContent.style.display = "none";
 	loadingScreen.style.display = "block";
+
+	await pause(5000);
+
+	socket.emit('get_modules_list', '');
+	socket.on('post_modules_list', data=>{
+		previousModuleList = data["data"];
+	})
+
+	if(newModuleList.length > previousModuleList.length)
+	{
+		for (const element of newModuleList)
+		{
+			if(element["name"] == data['moduleName'])
+			{
+				succes = true;
+				break;
+			}
+		}
+	}
+	else:
+	{
+		succes = false
+	}
+
+	if(succes == true)
+	{
+		document.location.href = '/moduleListing'
+	}
+	else
+	{
+		let notif = document.createElement('div');
+
+		notif.classList.add("row", "text-center");
+		notif.textContent = 'Erreur';
+
+		document.location.href = "/addModule"
+
+		pageContent.appendChild(notif);
+	}
+
+
 }
