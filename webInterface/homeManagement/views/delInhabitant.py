41f9ad0ec9cea17dfad58d03ff0013542b46079a
tetrew88 <donovanmaurice@laposte.net>
2022-04-04 04:35:49 +0200

    last update

--- webInterface/homeManagement/views/delInhabitant.py
+++ webInterface/homeManagement/views/delInhabitant.py
@@ -2,6 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def del_inhabitant(request):
-	template = "delRoom.html"
+	template = "delInhabitant.html"
 
 	return render(request, template)
